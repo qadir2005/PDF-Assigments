e7bc8f6601c395f6db2408f2110dc155101a02aa
qadir2005 <qadir20005@gmail.com>
2024-03-15 11:32:57 -0700

    first comment

---
+++ chap2.js
@@ -0,0 +1,63 @@
+//1)Declare a variable called username
+let username;
+//2)Declare a variable called myName & assign to it a string that represents your Full Name.
+let myName = "Abdul Qadir";
+//3). Write script to
+//a) Declare a JS variable, titled message.
+let message;
+//b) Assign “Hello World” to variable message
+message = "Hello World";
+//c) Display the message in alert box
+alert(message);
+//4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+let StudentName = "Muhammad Usman";
+let StudentAge = 18;
+let diploma = "CIT (Certification in Information Technology)";
+alert(StudentName);
+alert(StudentAge);
+alert(diploma);
+
+//5. Write a script to display the following alert using one JS variable:
+
+console.log("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+//Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: us string concatenation)
+
+let email = "qadir2005@gmail.com";
+alert(email);
+
+//7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+
+let book = "A smarter way to learn JavaScript";
+alert(book);
+
+//Write a script to display this in browser through JS
+
+document.write("Asslam o Alikum Everyone, My Name is Abdul Qadir and I am a Student of Smit");
+
+//9. Store following string in a variable and show in alert and browser through JS
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// Assigment 2 is completed
+
+//`Assigment 4 is started
+
+//1. Declare 3 variables in one statement
+
+let a = 1, let b = 2, let c = 3;
+
+//2. Declare 5 legal & 5 illegal variable names.
+//legal
+let name = "Usama";
+let secondName = "Khan";
+let isUserLogin = true;
+let numUsers = 23;
+let $dollor = 2454;
+let _underScore = 12;
+
+//illegal
+let if = "Nothig";
+let user 2 =: 23;
+let!not = "Absolutely Right"
+let value = 34;
