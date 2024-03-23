477f1f91ab3ce3f95af863f0bea3b49eb710cf8f
qadir2005 <qadir20005@gmail.com>
2024-03-23 08:55:36 -0700

    Some Changing

---
+++ Chapter5/pdf_5.js
@@ -0,0 +1,21 @@
+// Q1.Write a program that take 2 numbers & add them in a new variable.show the Result in your Browser
+
+// let firstValue = 39;
+// let secondValue = 43;
+// let sum = firstValue + secondValue;
+// document.write("Sum of " + firstValue + " and " + secondValue + " is " + sum)
+
+//Question No 2
+let randomValue;
+document.write("\"Value after variable declaration is: \" " + randomValue + "<br><br>");
+randomValue = 5;
+document.write("\" Inintial Value\" " + randomValue + "<br>" + "<br>")
+randomValue++;
+document.write("\"Value After Increament is \" " + randomValue + "<br>" + "<br>")
+randomValue += 7;
+document.write("\"Value After Addition is \" " + randomValue + "<br>" + "<br>")
+randomValue--;
+document.write("\"Value After Decreament is \" " + randomValue + "<br>" + "<br>")
+randomValue %= 3;
+document.write("\"The Remainder is \" " + randomValue + "<br>" + "<br>")
+
